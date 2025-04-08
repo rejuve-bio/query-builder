b8dc8ea94d9c8b2ed558f43c5d94aead0f554217
Yisehak Abreham <yisehak.aw@gmail.com>
2025-04-08 10:54:06 +0300

    fix a bug with an import

--- stories/Component.stories.js
+++ stories/Component.stories.js
@@ -1,5 +1,5 @@
 import { fn } from "@storybook/test";
-import Component from "../src/Component";
+import { Component } from "../src/Component";
 import "./style.css";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
