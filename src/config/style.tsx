fd4257e0fd38046ea3161ab1f61d83eea0707f59
Yisehak Abreham <yisehak.aw@gmail.com>
2025-04-08 13:54:48 +0300

    update story configuration

---
+++ src/config/style.tsx
@@ -0,0 +1,119 @@
+import { NodeClassDefinitionMap } from "../";
+
+const classes: NodeClassDefinitionMap = {
+  gene: {
+    form: "bg-purple-600 dark:bg-purple-700",
+    icon: "bg-purple-500 dark:bg-purple-900",
+    params:
+      "bg-purple-100 text-purple-700 dark:bg-purple-950 dark:text-purple-100",
+  },
+  protein: {
+    icon: "bg-violet-500  dark:bg-violet-900",
+    params:
+      "bg-violet-100 text-violet-700 dark:bg-violet-950 dark:text-violet-100",
+    form: "bg-violet-500 dark:bg-violet-700",
+  },
+  exon: {
+    form: "bg-indigo-500 dark:bg-indigo-700",
+    icon: "bg-indigo-500 dark:bg-indigo-900",
+    params:
+      "bg-indigo-100 text-indigo-700 dark:bg-indigo-950 dark:text-indigo-100",
+  },
+  transcript: {
+    icon: "bg-blue-500 dark:bg-blue-900",
+    params: "bg-blue-100 text-blue-700 dark:bg-blue-950 dark:text-blue-100",
+    form: "bg-blue-500 dark:bg-blue-700",
+  },
+  snp: {
+    icon: "bg-pink-500 dark:bg-pink-900",
+    params: "bg-pink-100 text-pink-700 dark:bg-pink-950 dark:text-pink-100",
+    form: "bg-pink-500 dark:bg-pink-700",
+  },
+  sv: {
+    icon: "bg-pink-500 dark:bg-pink-900",
+    params: "bg-pink-100 text-pink-700 dark:bg-pink-950 dark:text-pink-100",
+    form: "bg-pink-500 dark:bg-pink-700",
+  },
+  enhancer: {
+    icon: "bg-yellow-500 dark:bg-yellow-900",
+    params:
+      "bg-yellow-100 text-yellow-700 dark:bg-yellow-950 dark:text-yellow-100",
+    form: "bg-yellow-500 dark:bg-yellow-700",
+  },
+  super_enhancer: {
+    icon: "bg-yellow-500 dark:bg-yellow-900",
+    params:
+      "bg-yellow-100 text-yellow-700 dark:bg-yellow-950 dark:text-yellow-100",
+    form: "bg-yellow-500 dark:bg-yellow-700",
+  },
+  promoter: {
+    icon: "bg-orange-500 dark:bg-orange-900",
+    params:
+      "bg-orange-100 text-orange-700 dark:bg-orange-950 dark:text-orange-100",
+    form: "bg-orange-500 dark:bg-orange-700",
+  },
+  non_coding_rna: {
+    icon: "bg-amber-600 dark:bg-amber-900",
+    params: "bg-amber-100 text-amber-700 dark:bg-amber-950 dark:text-amber-100",
+    form: "bg-amber-600 dark:bg-amber-700",
+  },
+  pathway: {
+    icon: "bg-emerald-500 dark:bg-emerald-900",
+    params:
+      "bg-emerald-100 text-emerald-700 dark:bg-emerald-950 dark:text-emerald-100",
+    form: "bg-emerald-500 dark:bg-emerald-700",
+  },
+  go: {
+    icon: "bg-emerald-500 dark:bg-emerald-900",
+    params:
+      "bg-emerald-100 text-emerald-700 dark:bg-emerald-950 dark:text-emerald-100",
+    form: "bg-emerald-500 dark:bg-emerald-700",
+  },
+  uberon: {
+    icon: "bg-emerald-500 dark:bg-emerald-900",
+    params:
+      "bg-emerald-100 text-emerald-700 dark:bg-emerald-950 dark:text-emerald-100",
+    form: "bg-emerald-500 dark:bg-emerald-700",
+  },
+  clo: {
+    icon: "bg-emerald-500 dark:bg-emerald-900",
+    params:
+      "bg-emerald-100 text-emerald-700 dark:bg-emerald-950 dark:text-emerald-100",
+    form: "bg-emerald-500 dark:bg-emerald-700",
+  },
+  cl: {
+    icon: "bg-emerald-500 dark:bg-emerald-900",
+    params:
+      "bg-emerald-100 text-emerald-700 dark:bg-emerald-950 dark:text-emerald-100",
+    form: "bg-emerald-500 dark:bg-emerald-700",
+  },
+  efo: {
+    icon: "bg-emerald-500 dark:bg-emerald-900",
+    params:
+      "bg-emerald-100 text-emerald-700 dark:bg-emerald-950 dark:text-emerald-100",
+    form: "bg-emerald-500 dark:bg-emerald-700",
+  },
+  bto: {
+    icon: "bg-emerald-500 dark:bg-emerald-900",
+    params:
+      "bg-emerald-100 text-emerald-700 dark:bg-emerald-950 dark:text-emerald-100",
+    form: "bg-emerald-500 dark:bg-emerald-700",
+  },
+  motif: {
+    icon: "bg-lime-500 dark:bg-lime-900",
+    params: "bg-lime-100 text-lime-700 dark:bg-lime-950 dark:text-lime-100",
+    form: "bg-lime-500 dark:bg-lime-700",
+  },
+  tfbs: {
+    icon: "bg-lime-500 dark:bg-lime-900",
+    params: "bg-lime-100 text-lime-700 dark:bg-lime-950 dark:text-lime-100",
+    form: "bg-lime-500 dark:bg-lime-700",
+  },
+  tad: {
+    icon: "bg-rose-500 dark:bg-rose-900",
+    params: "bg-rose-100 text-rose-700 dark:bg-rose-950 dark:text-rose-100",
+    form: "bg-rose-500 dark:bg-rose-700",
+  },
+};
+
+export default classes;
--- stories/builder.stories.tsx
+++ stories/builder.stories.tsx
@@ -1,5 +1,9 @@
 import { ReactFlowProvider } from "@xyflow/react";
 import { QueryBuilder, QueryBuilderContext } from "../src/index";
+import { edgeDefinitions, nodeDefinitions } from "../src/config/schema";
+import classes from "../src/config/style";
+import Icons from "../src/config/icons";
+import formFields from "../src/config/form";
 import "./style.css";
 
 function QueryBuilderComponent() {
@@ -7,8 +11,11 @@ function QueryBuilderComponent() {
     <ReactFlowProvider>
       <QueryBuilderContext.Provider
         value={{
-          nodeDefinitions: [{ id: "gene", name: "Gene", category: "VIP" }],
-          edgeDefinitions: [],
+          nodeDefinitions,
+          edgeDefinitions,
+          forms: formFields,
+          style: classes,
+          icons: Icons,
         }}
       >
         <QueryBuilder nodes={[]} edges={[]} onSubmit={() => {}} />
